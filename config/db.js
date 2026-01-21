6168c25e4fdc48e98a1a9b39c29e5902dbb1cd7c
Dante.24 <106435890+Mohamed-Ly@users.noreply.github.com>
2026-01-21 23:25:46 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,15 +1,25 @@
 // db.js
+// const mysql = require('mysql2/promise');
+// require('dotenv').config();
+
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
+
+// module.exports = pool;
+
+
+// db.js
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
+// استخدام رابط DATABASE_URL مباشرة أسهل وأضمن للربط مع Railway
+const pool = mysql.createPool(process.env.DATABASE_URL);
 
 module.exports = pool;
